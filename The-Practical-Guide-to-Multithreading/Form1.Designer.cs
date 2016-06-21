87a7ee4a96c4069f6c5ef9e918d6eb337b8fe8cd
Eisner Lopez <elopeza@ucatolica.ac.cr>
2016-06-20 19:09:28 -0600

    SI

--- The-Practical-Guide-to-Multithreading/Form1.Designer.cs
+++ The-Practical-Guide-to-Multithreading/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_Proceso = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(22, 23);
+            this.txtStart.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtStart.MaxLength = 5;
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(164, 30);
+            this.txtStart.TabIndex = 0;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(257, 23);
+            this.txtEnd.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtEnd.MaxLength = 5;
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(164, 30);
+            this.txtEnd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, -5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, -5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "End:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 108);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(395, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btn_Proceso
+            // 
+            this.btn_Proceso.Location = new System.Drawing.Point(26, 137);
+            this.btn_Proceso.Name = "btn_Proceso";
+            this.btn_Proceso.Size = new System.Drawing.Size(395, 43);
+            this.btn_Proceso.TabIndex = 5;
+            this.btn_Proceso.Text = "Iniciar Proceso.";
+            this.btn_Proceso.UseVisualStyleBackColor = true;
+            this.btn_Proceso.Click += new System.EventHandler(this.btn_Proceso_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(25, 82);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 23);
+            this.lblStatus.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 207);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btn_Proceso);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximumSize = new System.Drawing.Size(471, 246);
+            this.MinimumSize = new System.Drawing.Size(471, 246);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_Proceso;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- The-Practical-Guide-to-Multithreading/Form1.cs
+++ The-Practical-Guide-to-Multithreading/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,51 @@ namespace The_Practical_Guide_to_Multithreading
         {
             InitializeComponent();
         }
+
+        // Delegates - same as function pointers.
+        // First line declares type, second declares variable
+        delegate void DelegateType(int x);
+        DelegateType TheDelegate;
+
+        // We store the Start and End values in class-variable,
+        // so that 'thread' actually does only processing.
+        int StartFrom, EndTo;
+
+        private void btn_Proceso_Click(object sender, EventArgs e)
+        {
+            // Set the delegate.
+            TheDelegate = MessageHandler;
+
+            StartFrom = Convert.ToInt32(txtStart.Text);
+            EndTo = Convert.ToInt32(txtEnd.Text);
+
+            progressBar1.Minimum = StartFrom;
+            progressBar1.Maximum = EndTo;
+
+            // Disable button, so that user cannot start again.
+            btn_Proceso.Enabled = false;
+
+            // Setup thread and start!
+            Thread MyThread = new Thread(ProcessRoutine);
+            MyThread.Start();
+        }
+
+        void MessageHandler(int nProgress)
+        {
+            lblStatus.Text = "Processing item: " + Convert.ToString(nProgress);
+            progressBar1.Value = nProgress;
+        }
+
+
+
+        void ProcessRoutine()
+        {
+            for (int nValue = StartFrom; nValue <= EndTo; nValue++)
+            {
+                // Only actual delegates be called with Invoke, and not functions!
+                this.Invoke(this.TheDelegate, nValue);
+            }
+        }
+
     }
 }
